7c9065a132e07b07e2b39a75d18571fcecfb55fd
mateuscampos72 <mateuscampos72@gmail.com>
2023-01-10 19:02:49 -0300

    exercicio_medidor_temp

---
+++ app.js
@@ -0,0 +1,12 @@
+const kelvin = 293;
+const celsius = kelvin - 273;
+//  eu poderia fazer está conta apenas trazendo a variavel e 
+// subtraindo pela diferença como está acima 
+let fahrenheit = celsius * (9/5) + 32;
+// let é uma variavel que permite a alteração de valores dentro do mesma
+
+
+
+// variavel que salva o numero 293
+
+fahrenheit =  fahrenheit.math.floor()
